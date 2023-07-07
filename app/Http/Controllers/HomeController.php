22249157a6f881822c6bf391e84c764fe710b9a0
Zaid al maghfur <91768583+ZAIDALMAGHFUR@users.noreply.github.com>
2023-07-08 00:11:00 +0700

    example dashboard admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
+use App\Models\Role;
+use App\Models\User;
+use App\Models\Location;
+use App\Models\PlasticType;
 use Illuminate\Http\Request;
+use App\Models\PlasticTypePrice;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
@@ -23,6 +30,46 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('dashboard.home');
+        $stats = Cache::remember('card-stats-' . auth()->id(), 60 * 1, fn () => $this->_getStats());
+
+        return view('dashboard.home', [
+            'stats' => $stats
+        ]);
+    }
+
+    private function _getStats()
+    {
+        return [
+            [
+                "label" => "Total Pengguna",
+                "value" => User::count(),
+                'icon' => 'sun'
+            ],
+            [
+                "label" => "Total Location",
+                "value" => Location::count(),
+                'icon' => 'info'
+            ],
+            [
+                "label" => "Total City",
+                "value" => City::count(),
+                'icon' => 'clipboard'
+            ],
+            [
+                "label" => "Total Plastic Type",
+                "value" => PlasticType::count(),
+                'icon' => 'grid'
+            ],
+            [
+                "label" => "Total Plastic Type Price",
+                "value" => PlasticTypePrice::count(),
+                'icon' => 'layout'
+            ],
+            [
+                "label" => "Total Role",
+                "value" => Role::count(),
+                'icon' => 'tag'
+            ],
+        ];
     }
 }
